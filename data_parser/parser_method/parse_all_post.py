0bdf94c3e8c3c396b25fc9ad7986b03752ac2699
davy <davy0718@gmail.com>
2016-09-02 23:02:11 +0800

    col updated_at is string

--- data_parser/parser_method/parse_all_post.py
+++ data_parser/parser_method/parse_all_post.py
@@ -69,7 +69,7 @@ def parse_id(category):
             'forum_name': post["forumName"],
             'school_name': "anonymous",
             'status': 'online',
-            'updated_at': datetime.today()
+            'updated_at': datetime.today().strftime('%Y%m%d')
           })
         else:
           PostData.objects.update_or_create(
@@ -84,7 +84,7 @@ def parse_id(category):
             'forum_name': post["forumName"],
             'school_name': post["school"],
             'status': 'online',
-            'updated_at': datetime.today()
+            'updated_at': datetime.today().strftime('%Y%m%d')
           })
 
       except Exception,e:
